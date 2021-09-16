7b4c4b2ab6dd01378737a57e186b9c92907a814a
leandrosimonato <leandrosimonato@gmail.com>
2021-09-16 11:00:33 -0300

    Criando o componente do ValueDescriptor.

---
+++ alganews-cms/src/components/ValueDescriptor/ValueDescriptor.styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  span.Description {
+    font-size: 14px;
+    font-weight: 500;
+    color: #274060;
+  }
+
+  span.Currency {
+    font-size: 14px;
+    font-weight: 500;
+    color: #274060;
+  }
+
+  span.Value {
+    font-size: 14px;
+    font-weight: 500;
+    color: #274060;
+  }
+`;
