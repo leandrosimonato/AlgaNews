ff5a0266cc459200ee41e7958f9359e8a341329c
leandrosimonato <leandrosimonato@gmail.com>
2021-09-20 15:08:27 -0300

    Setup do CircleChart.

--- alganews-cms/src/components/CircleChart/CircleChart.tsx
+++ alganews-cms/src/components/CircleChart/CircleChart.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import * as CC from './CircleChart.styles'
 
 export interface CircleChartProps {
@@ -9,6 +10,29 @@ export interface CircleChartProps {
 }
 
 function CircleChart(props: CircleChartProps) {
+  // Função que recupera a cor do Chart com base no tema!
+  const getThemeColor = () =>
+    props.theme === 'primary' ? '#09f' : '#274060'
+  
+  // Setup ( Configurações de cor, borda, etc. )
+  const THEME = getThemeColor()
+  const STROKE_WIDTH = props.strokeWidth || 8
+  const STROKE_COLOR = THEME
+
+  // Matemática da COISA!
+  const CENTER = props.size / 2
+  const RADIUS = props.size / 2 - STROKE_WIDTH / 2
+  const CIRCUMFERENCE = 2 * Math.PI * RADIUS
+
+  // Estado de offset!
+  const [offset, setOffset] = useState(CIRCUMFERENCE);
+
+  // Observador para animar o offset!
+  useEffect(() => {
+    const progressOffset = ((110 - props.progress) / 100) * CIRCUMFERENCE
+    setOffset(progressOffset);
+  },[setOffset, CIRCUMFERENCE, props.progress, offset])
+
   return <CC.Wrapper>
     todo: Circle Chart
   </CC.Wrapper>
