ad66d05715d9e9e9f0df5548f1c948dbfd1cc5d7
leandrosimonato <leandrosimonato@gmail.com>
2021-09-20 20:21:08 -0300

    Adicionando um Editor de texto em Markdown.

---
+++ alganews-cms/src/stories/MarkdownEditor.stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react';
+import MarkdownEditor, {
+  MarkdownEditorProps,
+} from '../components/MarkdownEditor';
+
+export default {
+  title: 'Example/MarkdownEditor',
+  component: MarkdownEditor,
+} as Meta;
+
+const Template: Story<MarkdownEditorProps> = (args) => (
+  <MarkdownEditor {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
