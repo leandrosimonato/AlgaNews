c425207a97a12eb2f0a2b6693f15947b51e7c122
leandrosimonato <leandrosimonato@gmail.com>
2021-09-20 19:37:29 -0300

    Exemplo funcional no StoryBook (com o TagInput).

--- alganews-cms/src/stories/TagInput.stories.tsx
+++ alganews-cms/src/stories/TagInput.stories.tsx
@@ -1,5 +1,7 @@
 import { Story, Meta } from '@storybook/react';
+import { useState } from 'react';
 import TagInput, { TagInputProps } from '../components/TagInput';
+import { Tag } from 'react-tag-input';
 
 export default {
   title: 'Example/TagInput',
@@ -17,3 +19,30 @@ Default.args = {
   placeholder: 'Insira as tags deste post',
   tags: [{ id: '1', text: 'JavaScript' }],
 };
+
+export const VariousTags = Template.bind({});
+VariousTags.args = {
+  placeholder: 'Insira as tags deste post',
+  tags: [
+    { id: '1', text: 'JavaScript' },
+    { id: '2', text: 'Java' },
+    { id: '3', text: 'Ruby on Rails' },
+    { id: '4', text: 'Python' },
+    { id: '5', text: 'JavaScript' },
+    { id: '6', text: 'JavaScript' },
+    { id: '7', text: 'JavaScript' },
+  ],
+};
+
+export function WorkingLiveExample() {
+  const [tags, setTags] = useState<Tag[]>([]);
+
+  return (
+    <TagInput
+      placeholder='Insira as tags deste post'
+      tags={tags}
+      onAdd={(tag) => setTags([...tags, tag])}
+      onDelete={(index) => setTags(tags.filter((tag, i) => i !== index))}
+    />
+  );
+}
