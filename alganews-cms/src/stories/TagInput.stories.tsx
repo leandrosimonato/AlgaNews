50fc1bee55006853e4625829376731ac927ed515
leandrosimonato <leandrosimonato@gmail.com>
2021-09-20 19:04:47 -0300

    Introdução ao TagInput.

---
+++ alganews-cms/src/stories/TagInput.stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react';
+import TagInput, { TagInputProps } from '../components/TagInput';
+
+export default {
+  title: 'Example/TagInput',
+  component: TagInput,
+} as Meta;
+
+const Template: Story<TagInputProps> = (args) => (
+  <div>
+    <TagInput {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  placeholder: 'Insira as tags deste post',
+  tags: [{ id: '1', text: 'JavaScript' }],
+};
