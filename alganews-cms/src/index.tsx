697827a6ee05a9a4c1ce0cfb31d0aeb968ada654
leandrosimonato <leandrosimonato@gmail.com>
2021-09-20 21:49:22 -0300

    Instalando e Configurando o React Router DOM.

--- alganews-cms/src/index.tsx
+++ alganews-cms/src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path={'/home'} exact>
+          <App />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
