d10051d941658244426f23673a0a23f4efba23c5
leandrosimonato <leandrosimonato@gmail.com>
2021-09-20 22:54:39 -0300

    Recuperando parâmetros da Query String.

--- alganews-cms/src/index.tsx
+++ alganews-cms/src/index.tsx
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import Home from './views/Home.view';
 import NotFound404 from './views/NotFound404.view';
 import Contact from './views/Contact.view';
+import UserView from './views/User.view';
+import CalcView from './views/Calc.view';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -24,6 +26,8 @@ ReactDOM.render(
         <Switch>
           <Route path={'/'} exact component={Home} />
           <Route path={'/contato'} exact component={Contact} />
+          <Route path={'/usuario/:userId'} component={UserView} />
+          <Route path={'/calc/:a/:b'} component={CalcView} />
           <Route component={NotFound404} />
         </Switch>
       </BrowserRouter>
