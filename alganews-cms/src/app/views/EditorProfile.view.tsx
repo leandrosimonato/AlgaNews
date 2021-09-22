1b34ceec8847b5ed7cdd9bd643291b693311b34d
leandrosimonato <leandrosimonato@gmail.com>
2021-09-21 23:25:08 -0300

    Escondendo dados sensíveis do usuário.

--- alganews-cms/src/app/views/EditorProfile.view.tsx
+++ alganews-cms/src/app/views/EditorProfile.view.tsx
@@ -1,7 +1,12 @@
+import EditorProfile from '../features/EditorProfile';
 import DefaultLayout from '../layouts/Default';
 
 function EditorProfileView() {
-  return <DefaultLayout>todo: editor profile view</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <EditorProfile hidePersonalData />
+    </DefaultLayout>
+  );
 }
 
 export default EditorProfileView;
