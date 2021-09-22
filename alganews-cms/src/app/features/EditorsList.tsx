5c2f70cb407f8a19a262382281fbc0e622f42973
leandrosimonato <leandrosimonato@gmail.com>
2021-09-21 22:54:36 -0300

    Estilizando um componente de uma biblioteca externa.

--- alganews-cms/src/app/features/EditorsList.tsx
+++ alganews-cms/src/app/features/EditorsList.tsx
@@ -1,15 +1,22 @@
 import styled from 'styled-components';
 import Profile from '../components/Profile';
 
-
 export default function EditorsList() {
   return (
     <EditorsListWrapper>
-      <Profile name="Leandro Simonato" description="Programador há 1 ano" />
-      <Profile name="Ana Carolina" description="Programador há 1 ano" />
-      <Profile name="Antonella" description="Programador há 5 ano" />
-      <Profile name="Henrique" description="Programador há 6 ano" />
-      <Profile name="Rafael Borges" description="Programador há 10 ano" />
+      <Profile
+        editorId={1}
+        name='Leandro Simonato'
+        description='editor há 8 anos'
+      />
+      <Profile
+        editorId={2}
+        name='Ana Carolina'
+        description='editor há 2 anos'
+      />
+      <Profile editorId={3} name='Antonella' description='editor há 2 anos' />
+      <Profile editorId={4} name='Henrique' description='editora há 6 anos' />
+      <Profile editorId={5} name='Rafael Borges' description='editor há 2 meses' />
     </EditorsListWrapper>
   );
 }
---
+++ alganews-cms/src/app/views/EditorProfile.view.tsx
@@ -0,0 +1,7 @@
+import DefaultLayout from '../layouts/Default';
+
+function EditorProfileView() {
+  return <DefaultLayout>todo: editor profile view</DefaultLayout>;
+}
+
+export default EditorProfileView;
